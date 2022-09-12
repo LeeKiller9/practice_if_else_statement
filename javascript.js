09a44a522d9b7cc85ec84a08afa254ca250e7c1e
LeeKiller9 <112848883+LeeKiller9@users.noreply.github.com>
2022-09-12 11:52:05 +0700

    up index styles javascript file

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function check_ans(){
+    let value = prompt('Insert your answer');
+    if (value === 'ECMAScript' || value === 'ecmascript') {
+        alert('Right!');
+    } else {
+        alert("You don't know? ECMAScript!");
+    }
+}
+
+
+
